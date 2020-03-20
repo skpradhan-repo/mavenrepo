44bc0cdc5a4c1e1bde61b283fe73c782dd2d9680
Samaresh Pradhan <samareshpro@gmail.com>
2020-03-20 19:58:53 +0530

    buyer-parent-module v1.0.0 added

---
+++ parent-buyer-app/security-buyer-app/src/main/java/com/sap/buyer/App.java
@@ -0,0 +1,13 @@
+package com.sap.buyer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
